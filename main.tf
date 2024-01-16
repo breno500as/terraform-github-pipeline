738afcf3120a8110a7bfeb54bd002cff0aae81e0
breno500as <breno500as@gmail.com>
2024-01-16 15:14:31 -0300

    up

---
+++ main.tf
@@ -0,0 +1,31 @@
+terraform {
+  required_version = "1.6.6"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.32.0"
+    }
+  }
+
+
+  backend "s3" {
+    bucket = "breno500as-terraform-remote-state"
+    key    = "aws-vpc/terraform.tfstate"
+    region = "us-east-1"
+  }
+
+
+}
+
+provider "aws" {
+
+  region = "us-east-1"
+
+  default_tags {
+    tags = {
+      owner      = "breno500as"
+      managed-by = "terraform"
+
+    }
+  }
+}
